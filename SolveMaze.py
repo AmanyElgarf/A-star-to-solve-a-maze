99abeaa46e51cdff9fa25fda551f1fa42fcc7f8e
AmanyElgarf <amany.elgarf@rutgers.edu>
2019-07-08 12:19:28 -0400

    A forward is complete

--- SolveMaze.py
+++ SolveMaze.py
@@ -1,8 +1,6 @@
-from Maze import Maze
-from PriorityQueue import PriorityQueue
-from Node import Node
 
 import math
+
 class SolveMaze:
 
     def forward_A_star(self, open_list, closed_list, start_node, goal_node, counter):
@@ -10,22 +8,34 @@ class SolveMaze:
         while goal_node.f > open_list.get_min().f:
             current = open_list.del_min()
             closed_list.add(current)
-            child = Node()
-            child = None
+            child = start_node
+
             for i in range(0, 4):
-                if i == 0: child = current.right_child
-                if i ==1: child = current.left_child
-                if i ==2: child = current.top_child
-                if i ==3: child = current.down_child
-                if child is None: i += 1
-                if child.counter < counter:
+                if i == 0:
+                    child = start_node.right_child
+                    if child is None:
+                        i += 1
+                if i == 1:
+                    child = start_node.left_child
+                    if child is None:
+                        i += 1
+                if i == 2:
+                    child = start_node.top_child
+                    if child is None:
+                        i += 1
+                if i == 3:
+                    child = start_node.down_child
+                    if child is None:
+                        i += 1
+
+                if child.search < counter:
                     child.g = math.inf
-                    child.counter = counter
-                if child.g > current.g + child.cost:
-                    child.g = current.g + child.cost
-                    child.parent = [current.x, current.y]
+                    child.search = counter
+
+                if child.g > start_node.g + child.cost:
+                    child.g = start_node.g + child.cost
+                    child.parent = [start_node.x, start_node.y]
                     child.h = child.manhattan_distance(child, goal_node)
-                    child.f = child.g + child.h
                     open_list.insert(child)
 
 
