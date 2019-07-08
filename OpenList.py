011cdc3058182df66e807690375f81e66b839a7d
AmanyElgarf <amany.elgarf@rutgers.edu>
2019-07-08 01:07:19 -0400

    AStar is complete

--- OpenList.py
+++ OpenList.py
@@ -1,7 +1,8 @@
-import math
-class PriorityQueue:
+from Node import Node
+
+class openList:
     def __init__(self):
-        self.heap = [0]
+        self.heap = [Node(-9,-9)]
         self.current_size = 0
 
     def is_empty(self):
@@ -10,13 +11,13 @@ class PriorityQueue:
         return False
 
     def parent_pos(self, child_pos):
-        return math.floor(int(child_pos/2))
+        return (int(child_pos/2))
 
     def left_child_pos(self, parent_pos):
-        return math.floor(parent_pos*2)
+        return int(parent_pos*2)
 
     def right_child_pos(self, parent_pos):
-        return math.floor((parent_pos * 2) + 1)
+        return int((parent_pos * 2) + 1)
 
     def is_leaf(self, pos):
         if pos <= int(self.current_size/2):
@@ -27,18 +28,22 @@ class PriorityQueue:
         self.heap[parent_pos], self.heap[child_pos] = self.heap[child_pos], self.heap[parent_pos]
 
     def swim(self, child_pos):
-        while self.heap[self.parent_pos(child_pos)] > self.heap[child_pos] and child_pos > 1:
+        while self.heap[self.parent_pos(child_pos)].f >= self.heap[child_pos].f and child_pos > 1:
+            if self.heap[self.parent_pos(child_pos)].f == self.heap[child_pos].f:
+                if self.heap[self.parent_pos(child_pos)].g == self.heap[child_pos].g:
+                    self.swap(self.parent_pos(child_pos), child_pos)
+                    break
             self.swap(self.parent_pos(child_pos), child_pos)
             child_pos = self.parent_pos(child_pos)
 
     def sink(self, parent_pos):
-        while (self.heap[parent_pos] > self.heap[self.left_child_pos(parent_pos)] or
-               self.heap[self.right_child_pos(parent_pos)]) \
+        while (self.heap[parent_pos].f > self.heap[self.left_child_pos(parent_pos)].f or
+               self.heap[self.right_child_pos(parent_pos)].f) \
                 and self.is_leaf(parent_pos) is False:
-            if self.heap[parent_pos] > self.heap[self.left_child_pos(parent_pos)]:
+            if self.heap[parent_pos].f > self.heap[self.left_child_pos(parent_pos)].f:
                 self.swap(parent_pos, self.left_child_pos(parent_pos))
                 parent_pos = self.left_child_pos(parent_pos)
-            elif self.heap[parent_pos] > self.heap[self.right_child_pos(parent_pos)]:
+            elif self.heap[parent_pos].f > self.heap[self.right_child_pos(parent_pos)].f:
                 self.swap(parent_pos, self.right_child_pos(parent_pos))
                 parent_pos = self.right_child_pos(parent_pos)
 
@@ -57,7 +62,32 @@ class PriorityQueue:
         self.sink(1)
         return min
 
-
     def print(self):
-        for i in range (1, self.current_size+1):
-            print(self.heap[i])
+        for i in range(1, self.current_size+1):
+            print(self.heap[i].f)
+
+k = openList()
+cell = Node(1, 1)
+cell.update_g(6)
+cell.update_h(6)
+
+cell1 = Node(1, 2)
+cell1.update_g(4)
+cell1.update_h(3)
+
+
+cell2 = Node(1, 3)
+cell3 = Node(4, 5)
+cell3.update_g(3)
+cell3.update_h(4)
+
+
+
+k.insert(cell)
+k.insert(cell1)
+k.insert(cell2)
+k.insert(cell3)
+
+k.del_min()
+k.print()
+
