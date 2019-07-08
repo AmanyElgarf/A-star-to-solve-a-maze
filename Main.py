76f8d5055c185a29188ba3025368ceeed831e80c
AmanyElgarf <amany.elgarf@rutgers.edu>
2019-07-08 14:00:51 -0400

    Amany will update main on wednesday

--- Main.py
+++ Main.py
@@ -1,22 +1,9 @@
-# from PriorityQueue import PriorityQueue
-#
-# x = PriorityQueue()
-# x.insert(10)
-# x.insert(9)
-# x.insert(4)
-#
-# x.insert(3)
-#
-# x.insert(1)
-#
-# x.print()
-
 import random
 from SolveMaze import SolveMaze
 from Maze import Maze
-from PriorityQueue import PriorityQueue
-from Node import Node
+from OpenList import OpenList
 import math
+
 
 class Main:
     def __init__(self):
@@ -24,7 +11,6 @@ class Main:
         # type maze where every thing is unblocked
         self.agent_maze = Maze().generate_blank_maze(3)
         self.actual_maze = Maze().generate_actual_maze(3)
-
 
     def traverse_path(self, goal_node):
         array = []
@@ -36,6 +22,7 @@ class Main:
 
 
     def main(self):
+        # generating start and goal positions
         array = []
         for x in range(4):
             random.randint(0, 5)
@@ -44,46 +31,48 @@ class Main:
         start_y = array[1]
         goal_x = array[2]
         goal_y = array[3]
-        start_node = agent_maze[start_x][start_y]
-        goal_node = agent_maze[goal_x][goal_y]
+        start_node = self.agent_maze[start_x][start_y]
+        goal_node = self.agent_maze[goal_x][goal_y]
 
-
-        while start_x != goal_x and start_y != goal_y:
-            self.counter +=1
-            start_node.g = 0
-            start_node.counter = self.counter
-            start_node.parent = [-1, -1]
-            goal_node.g = math.inf
-            goal_node.counter = self.counter
-
-
-            open_list = PriorityQueue()
+        while start_x != goal_x or start_y != goal_y:
+            self.counter += 1
+            start_node.update_g (0)
+            start_node.update_search(self.counter)
+            start_node.update_parent([-1, -1])
+            goal_node.update_g(math.inf)
+            goal_node.update_search(self.counter)
+            open_list = OpenList()
             open_list.insert(start_node)
             closed_list = set()
             SolveMaze().forward_A_star(open_list, closed_list, start_node, goal_node, self.counter)
 
             if open_list.is_empty is True:
-                print("No path Possible")
+                print("I can't reach the target")
                 return
 
             path = self.traverse_path(goal_node)
 
+
+
+
+            # AMANY WILL EDIT ON WEDNESDAY
             parent = array[len(array)-1]
             current = parent
             for i in range(len(path) - 2):
                 current = path[i]
                 # if actual_maze[parent[0], parent[1]].left_child == actual_maze[current[0], current[1]]:
-                if actual_maze[current[0], current[1]].cost != 1:
-                    start_node = agent_maze[parent[0], parent[1]]
-                    agent_maze[current[0], current[1]].cost = math.inf
+                if self.actual_maze[current[0], current[1]].cost != 1:
+                    start_node = self.agent_maze[parent[0], parent[1]]
+                    self.agent_maze[current[0], current[1]].cost = math.inf
                     break
                 else:
                     parent = current
 
-
-            if agent_maze[parent[0], parent[1]] == goal_node:
+            if self.agent_maze[parent[0], parent[1]] == goal_node:
                 break
 
+        print("I reached the goal")
+        return
 
 
 
