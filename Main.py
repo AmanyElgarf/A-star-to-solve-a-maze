d94f352f8e6d0c800b2f00e7755b6a3e8d022c61
Amany Elgarf <35881779+AmanyElgarf@users.noreply.github.com>
2019-07-13 01:26:37 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,37 +2,21 @@ import random
 from SolveMaze import SolveMaze
 from Maze import Maze
 from OpenList import OpenList
-import math
 
 
 class Main:
-    def __init__(self):
-        pass
-
-    def traverse_path(self, goal_node):
+    def traverse_path(self, goal_node, start_node):
         path = []
         index = [goal_node.x, goal_node.y]
         currentNode = goal_node
-        while index != [-1, -1]:
+        while currentNode is not start_node:
             path.append(index)
-            for i in range(0,4):
-                parentNode = currentNode.traverse_children(i)
-                if parentNode is not None:
-                    if currentNode.parent == [parentNode.x, parentNode.y]:
-                        currentNode = parentNode
-                        index = [parentNode.x, parentNode.y]
-                        break
-                    elif currentNode.parent == [-1, -1]:
-                        index = [-1, -1]
-                        break
-        #print("Path: ")
-        #for a in reversed(path):
-        #    print( "(", a[0], ", ", a[1], ")")
-        #print()
+            currentNode = currentNode.parent
+            index = [currentNode.x, currentNode.y]
+        path.append([start_node.x, start_node.y])
         return path
 
-    def main(self, size):
-
+    def main_A_forward(self, size):
         counter = 0
         agent_maze = Maze().generate_blank_maze(size)
         actual_maze = Maze().generate_actual_maze(size)
@@ -43,10 +27,8 @@ class Main:
             start_node_actual = actual_maze[start_node.x][start_node.y]
             goal_node_actual = actual_maze[goal_node.x][goal_node.y]
             if (start_node_actual.cost == 1) & (goal_node_actual.cost == 1) & (start_node != goal_node):
-                print(start_node, goal_node)
                 break
 
-        #tell agent map whether immediate cells are blocked or not
         for i in range(0, 4):
             if i == 0 and start_node_actual.right_child is not None:
                 start_node.right_child.cost = start_node_actual.right_child.cost
@@ -59,55 +41,51 @@ class Main:
 
         solvedMaze = []
 
-        while start_node != goal_node:
+        while start_node is not goal_node:
             counter += 1
             start_node.update_g(0)
             start_node.update_h(goal_node)
             start_node.update_search(counter)
-            start_node.update_parent([-1, -1])
 
             goal_node.update_g(float("inf"))
-            goal_node.update_h(goal_node)
             goal_node.update_search(counter)
 
             open_list = OpenList()
             open_list.insert(start_node)
             closed_list = set()
 
-            SolveMaze().forward_A_star(open_list, closed_list, goal_node, counter)
+            SolveMaze().forward_A_star(open_list, closed_list, goal_node, agent_maze)
 
-            if open_list.is_empty is True:
+            if open_list.is_empty is True and goal_node not in closed_list:
                 print("I can't reach the target")
                 return
 
-            path = self.traverse_path(goal_node)
+            path = self.traverse_path(goal_node, start_node)
 
             parentIndex = path.pop()
-            parentNode = actual_maze[ parentIndex[0] ][ parentIndex[1] ]
+            parentNode = actual_maze[parentIndex[0]][parentIndex[1]]
             solvedMaze.append(parentNode)
 
+            # compare with actual maze
             for currentIndex in reversed(path):
                 currentNode = actual_maze[currentIndex[0]][currentIndex[1]]
-
                 isChild = False
-                for j in range(0,4):
+                for j in range(0, 4):
                     child = parentNode.traverse_children(j)
                     if child is not None:
-                        child.print()
                         if child == currentNode:
                             isChild = True
                             break
                 if isChild:
                     if currentNode.cost != 1:
                         start_node = agent_maze[ parentNode.x][ parentNode.y]
-                        agent_maze[ currentNode.x][ currentNode.y ].cost = float("inf")
+                        agent_maze[currentNode.x][currentNode.y].cost = float("inf")
                         solvedMaze.pop()
                         break
                     else:
                         parentNode = currentNode
                         solvedMaze.append(parentNode)
                 else:
-                    print("A* search error, parent does not have such child")
                     return
 
             if agent_maze[parentNode.x][parentNode.y] == goal_node:
@@ -115,8 +93,3 @@ class Main:
                 for a in solvedMaze:
                     a.print()
                 return
-
-
-Main().main(3)
-
-
