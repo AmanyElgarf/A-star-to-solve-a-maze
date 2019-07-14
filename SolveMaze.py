ae45373002ff4537e0cfa0b15f818a7de14e32c1
Amany Elgarf <35881779+AmanyElgarf@users.noreply.github.com>
2019-07-14 19:47:38 -0400

    Update SolveMaze.py

--- SolveMaze.py
+++ SolveMaze.py
@@ -36,6 +36,47 @@ class SolveMaze:
         if open_list.is_empty() is True and goal_node not in closed_list:
             return 0
         return 1
+    
+        def backward_A_star(self, start_node,  goal_node, actual_maze, w):
+        goal_node.update_g(0)
+        goal_node.update_h(start_node)
+        start_node.update_g(float("inf"))
+        open_list = OpenList()
+        open_list.insert(goal_node)
+
+        closed_list = set()
+        while open_list.is_empty() is False:
+            current = open_list.del_min()
+
+            if current == start_node:
+                return
+            if current in closed_list:
+                continue
+            closed_list.add(current)
+
+            w.inClosed(current)
+            #w.master.update()
+            for i in range(0, 4):
+                child = current.traverse_children(i)
+                if child is None: continue
+                if child.cost != 1: continue
+                if child in closed_list: continue
+                if open_list.contains(child) is False:
+                    child.update_g(float("inf"))
+                    child.update_h(start_node)
+                if child.g > current.g + child.cost:
+                    newG = current.g + child.cost
+                    child.update_g(newG)
+                    child.parent = actual_maze[current.x][current.y]
+                    open_list.insert(child)
+                    w.inOpen(child)
+                    # w.master.update()
+        w.master.update()
+
+        if open_list.is_empty() is True and goal_node not in closed_list:
+            return 0
+
+        return 1
 
     def adaptive_A_star(self, start_node, goal_node, lastClosedList, w):
         lastGoalCost = goal_node.g
